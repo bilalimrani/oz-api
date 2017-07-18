f3276d07c2a8970ebba965159386a5146cb111e9
al-babar <al@appdesigncompany.com>
2017-07-18 20:14:29 +0500

    c

--- oz-api/register/register.js
+++ oz-api/register/register.js
@@ -56,7 +56,7 @@ router.post('/register',registerValidator,(req,res)=>{
 			
         }else{
             res.json(REST_API_STATUS_CODE.already_reported);
-        }
+        } 
     })
     .catch((err)=>{
         res.status(400).json(REST_API_STATUS_CODE.badrequest);
