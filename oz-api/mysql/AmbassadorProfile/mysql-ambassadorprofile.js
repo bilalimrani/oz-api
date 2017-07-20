9281e8ab840fbab4506d190d11f97050c73af7bb
al-babar <al@appdesigncompany.com>
2017-07-20 12:02:07 +0500

    Changes

--- oz-api/mysql/AmbassadorProfile/mysql-ambassadorprofile.js
+++ oz-api/mysql/AmbassadorProfile/mysql-ambassadorprofile.js
@@ -6,8 +6,8 @@ const mysql = require('../mysql-client');
 var multer = require('multer');
 var thumb = require('node-thumbnail').thumb;
 console.log("mysql-AmbassadorProfile file")
-let path = "./bin/upload";
-let thumbPath = "./bin/thumbnail";
+let path = "var/www/upload";
+let thumbPath = "var/www/thumbnail";
 var img_path = null; 
 var AmbassadorProfile = function(){
 	return{
