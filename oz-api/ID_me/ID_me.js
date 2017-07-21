0198ad793f745116c53d3fe571c081ed33b37b23
al-babar <al@appdesigncompany.com>
2017-07-21 15:06:39 +0100

    push

--- oz-api/ID_me/ID_me.js
+++ oz-api/ID_me/ID_me.js
@@ -5,7 +5,7 @@ const verifier_function = require('../verifier');
 
 
 router.get('/ID_me', (req,res)=>{
-	console.log("ID.me",req)
+	console.log("ID.me",req.body, req.params, req.query)
 	res.send({message : "ID.me"})
 })
 
