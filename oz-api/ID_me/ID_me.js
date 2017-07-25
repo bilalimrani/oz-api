55504d1d195f29ffc030207feb5e59e9284967bd
al-babar <al@appdesigncompany.com>
2017-07-25 12:39:51 +0100

    auth

--- oz-api/ID_me/ID_me.js
+++ oz-api/ID_me/ID_me.js
@@ -3,28 +3,33 @@ const router = express.Router()
 const verifier_function = require('../verifier');
 const curl = require('curl')
 var request = require('request');
-
+const app = express();
 var url = 'https://api.id.me/oauth/token';
 
-
+let code;
 router.get('/ID_me', (req,res)=>{
-	let query_data = req.query.code
+	code = req.query.code
+	res.redirect('/IDme_auth');
+
+})
+
+app.post('/IDme_auth', (req, res)=>{
 
 	let body = {
 		client_d : '2a4020a6d1b4fc5d721ed95be614e879',
-		code : req.query.code,
+		code : code,
 		client_secret : '27bf2978791fb27a8b6ee84a38688741',
 		redirect_uri : 'https://oz-dev.crewlogix.com/mobile/ID_me'
 	}
 
 	let options = {
 		client_d : '2a4020a6d1b4fc5d721ed95be614e879',
-		code : req.query.code,
+		code : code,
 		client_secret : '27bf2978791fb27a8b6ee84a38688741',
 		redirect_uri : 'https://oz-dev.crewlogix.com/mobile/ID_me'
 	}
 
-	console.log("req",options)
+	
 
 	let response1;
 	request({url : url, option : options}, function (error, response, body) {
@@ -32,7 +37,6 @@ router.get('/ID_me', (req,res)=>{
 		});
 
 	res.send(response1)
-
 })
 
 module.exports = router;
