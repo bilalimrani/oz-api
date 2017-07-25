154a005e31bbed1a99bd66d16b84253deee190ef
al-babar <al@appdesigncompany.com>
2017-07-25 13:22:57 +0100

    id

--- oz-api/ID_me/ID_me.js
+++ oz-api/ID_me/ID_me.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const verifier_function = require('../verifier');
-const curl = require('curl')
+
 var request = require('request');
 const app = express();
 var url = 'https://api.id.me/oauth/token';
 let code;
+var curl = require('curlrequest');
 
 router.get('/ID_me', (req,res)=>{
 	code = req.query.code
+	res.redirect('/mobile/ID_me');
+
+
+})
+
+router.post('/mobile/IDme_auth', (req, res)=>{
 
 	let body = {
-		url : 'https://api.id.me/oauth/token',
 		client_d : '2a4020a6d1b4fc5d721ed95be614e879',
 		code : code,
 		client_secret : '27bf2978791fb27a8b6ee84a38688741',
@@ -23,21 +29,20 @@ router.get('/ID_me', (req,res)=>{
 		client_d : '2a4020a6d1b4fc5d721ed95be614e879',
 		code : code,
 		client_secret : '27bf2978791fb27a8b6ee84a38688741',
-		redirect_uri : 'https://oz-dev.crewlogix.com/mobile/ID_me'
+		redirect_uri : 'https://oz-dev.crewlogix.com/mobile/ID_me',
+		
 	}
 
 	
 
 	let response1;
-	request(options, function (error, response, body) {
-
-			console.log("response", response)
-		    response1 = response
-		    res.send(response1)
+	curl.request(options, function (err, parts) {
+	    parts = parts.split('\r\n');
+	    var data = parts.pop()
+	      , head = parts.pop();
 	});
-	res.send(response1)
-	
-})
 
+	res.send(response1)
+})
 
 module.exports = router;
