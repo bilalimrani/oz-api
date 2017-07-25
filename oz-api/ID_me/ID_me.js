41204e5a830522837d729d3befe62b1e163f81f0
al-babar <al@appdesigncompany.com>
2017-07-25 09:44:51 +0100

    idme12

--- oz-api/ID_me/ID_me.js
+++ oz-api/ID_me/ID_me.js
@@ -6,14 +6,12 @@ const verifier_function = require('../verifier');
 
 router.get('/ID_me', (req,res)=>{
 	console.log("ID.me",req.body, req.params, req.query,res)
+	let query_data = req.query.code
 
-
-	let data = {
-		body : req.body,
-		params : req.params,
-		query : req.query
+	let data1 = {
+		query : query_data,
 	}
-	res.send({message : "ID.me", response : data})
+	res.send({message : "ID.me", response : data1})
 })
 
 module.exports = router;
