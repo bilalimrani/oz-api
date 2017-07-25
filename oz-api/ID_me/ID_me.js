732cfa80a8d58340d592b2e183c3a001a8be0190
al-babar <al@appdesigncompany.com>
2017-07-25 13:32:53 +0100

    id

--- oz-api/ID_me/ID_me.js
+++ oz-api/ID_me/ID_me.js
@@ -8,15 +8,15 @@ var url = 'https://api.id.me/oauth/token';
 let code;
 var curl = require('curlrequest');
 
+
+
+var curlify = require('request-as-curl'),
+    request = require('express'),
+    data = {data: 'data'};
+
+
 router.get('/ID_me', (req,res)=>{
 	code = req.query.code
-	res.redirect('/mobile/ID_me');
-
-
-})
-
-router.post('/mobile/IDme_auth', (req, res)=>{
-
 	let body = {
 		client_d : '2a4020a6d1b4fc5d721ed95be614e879',
 		code : code,
@@ -30,19 +30,22 @@ router.post('/mobile/IDme_auth', (req, res)=>{
 		code : code,
 		client_secret : '27bf2978791fb27a8b6ee84a38688741',
 		redirect_uri : 'https://oz-dev.crewlogix.com/mobile/ID_me',
-		
+
 	}
 
 	
 
 	let response1;
-	curl.request(options, function (err, parts) {
-	    parts = parts.split('\r\n');
-	    var data = parts.pop()
-	      , head = parts.pop();
+	var req = request('https://api.id.me/oauth/token', {method: 'POST', json: options}, function (error, response, expected) {
+	    // Note: req.req! 
+	    response1 = response
+	    // curl 'http://google.com' -H 'accept: application/json' -H 'content-type: application/json' -H 'connection: keep-alive' --data '{"data":"data"}' --compressed 
 	});
 
-	res.send(response1)
+	res.send({"message" : response1})
+
+
 })
+
 
 module.exports = router;
