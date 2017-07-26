0c2ed60e858d50e89a5374ed9e6a76b54ae4f80c
al-babar <al@appdesigncompany.com>
2017-07-26 09:44:32 +0100

    id

--- oz-api/ID_me/ID_me.js
+++ oz-api/ID_me/ID_me.js
@@ -16,7 +16,7 @@ router.get('/ID_me',  (req,res)=>{
 router.get('/verification', verifier_function.verifier, (req, res)=>{
 	let IDme_Url = 'https://api.id.me/oauth/authorize?client_id=2a4020a6d1b4fc5d721ed95be614e879&redirect_uri=https://oz-dev.crewlogix.com/mobile/ID_me&response_type=code&scope=military&state=488e864b'
 	request(IDme_Url, (request,response)=>{
-		res.send("data")
+		res.send("data",request)
 	})
 
 
