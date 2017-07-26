a525d336891fe21f0331d97ef69f2a4881e49635
al-babar <al@appdesigncompany.com>
2017-07-26 13:28:02 +0500

    Changes

--- responses.js
+++ responses.js
@@ -3,6 +3,7 @@ var responses = {
 	'already_reported' : {responseCode: 208, message:"Account Already Exist",response:{}},
 	'created' : {responseCode : 201, message: "Insert Record Sucessfully Please verify your account for activation",response:{}},
 	'no_content_found' : {responseCode : 204, message: "No Record Found",response:{}},
+	'no_content_found1' : {responseCode : 204, message: "No Record Found",response:[]},
 	'ok_userAlreadyLogIn' : {responseCode : 200,message : "User Already Login",response:{}},
 	'sucess' : {responseCode : 200,message : "sucess",response:{}},
 	'unauthorized' : {responseCode : 401 , message : "You are not authorize for this request",response : {}},
