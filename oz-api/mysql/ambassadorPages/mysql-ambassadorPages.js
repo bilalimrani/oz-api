0c6d9ce6518db4d9fd0a77cad2707aed64987285
al-babar <al@appdesigncompany.com>
2017-07-28 13:09:24 +0100

    questionare

--- oz-api/mysql/ambassadorPages/mysql-ambassadorPages.js
+++ oz-api/mysql/ambassadorPages/mysql-ambassadorPages.js
@@ -19,6 +19,30 @@ var ambassadorPage = function(data){
 					}
 				})
 			})
+		},
+
+		questionarepage : function(){
+			let query = 'select questions.description, questions.id, answer_options.question_id, answer_options.option, answer_options.is_correct from questions left join answer_options on questions.id = answer_options.question_id'
+
+
+			return new Promise((resolve, reject)=>{
+				databaseUtil.getSingleRecord(mysql, query, (err, res)=>{
+					if (err) {
+						reject(err)
+					}
+					else{
+						let arr = []
+						let questions = []
+						res.forEach((value)=>{
+
+							let allQuestion = value.description;
+							questions.push(allQuestion)
+							
+						})
+						resolve(questions)
+					}
+				})
+			})
 		}
 	}
 }
